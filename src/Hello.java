5d0f4d45772bd21b4b241865178981378541ad3c
Serine Galstyan <serine.galstyan98@gmail.com>
2025-05-07 12:09:01 +0400

    2nd homework

--- src/Hello.java
+++ src/Hello.java
@@ -2,15 +2,24 @@ public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello,Picsart Academy!");
-        System.out.println("Hello,Picsart!");
-        System.out.println("Hello, Academy!");
+        boolean second_homework = true;
+        char name_initial = 'S';
+        //char name_initial_2 = "S";
+        byte coffee_limit = 4;
+        short calories_per_day = 2000;
+        int order_number = 30000;
+        long project_code_rows = 100000L;
+        //long rows = 100000l;
+        float human_temperature = 36.6f;
+        double devision_result = 0.00000009;
 
-        int a = 5;
-        int b = 15;
-        int sum = a + b;
-
-        System.out.println(sum);
-
+        System.out.println("It's homework of the second lesson. " + second_homework);
+        System.out.println("First name initial: " + name_initial);
+        System.out.println("Daily coffee limit is " + coffee_limit);
+        System.out.println("Required calories per day is " + calories_per_day);
+        System.out.println("My order number is " + order_number);
+        System.out.println("The amount of rows in project code " + project_code_rows);
+        System.out.println("Normal human temperature is " + human_temperature);
+        System.out.println("The result of dividing 9 by 100.000.000 is " + devision_result);
     }
 }
